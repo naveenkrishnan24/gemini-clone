1cb1103d92510cf1224a01d3546c4b4873b39127
naveenkrishnan24 <jerrynk47@gmail.com>
2024-04-04 21:50:32 +0530

    gemini-clone is added to git

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Sticky from 'react-stickynode'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+
+
+const App = () => {
+  return (
+    <>
+    
+      <Sidebar></Sidebar>
+    
+    <Main></Main>
+    </>
+  )
+}
+
+export default App
+
